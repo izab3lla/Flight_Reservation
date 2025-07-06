2995db3b7600f3ce0407453ccebe9849b2d96609
Izabella Araujo <izabellaa244@gmail.com>
2025-07-05 22:33:45 -0400

    adicionando setters

--- client.py
+++ client.py
@@ -5,3 +5,11 @@
 class Client:
     def __init__(self, name: str):
         self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
